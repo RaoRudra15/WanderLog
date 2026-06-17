af18c076f4bcb6a026019200152ac28b86e72fc1
RaoRudra15 <162536007+RaoRudra15@users.noreply.github.com>
2026-06-17 19:39:29 +0000

    Build Login Page

--- frontend/wanderlog/src/App.jsx
+++ frontend/wanderlog/src/App.jsx
@@ -1,22 +1,37 @@
-import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom';
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/home/Home";
 
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return (
+    <Navigate
+      to={isAuthenticated ? "/dashboard" : "/login"}
+      replace
+    />
+  );
+};
+
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/dashboard" exact element={<Home/>}/>
-          <Route path="/login" exact element={<Login/>}/>
-          <Route path="/signup" exact element={<SignUp/>}/>
-        </Routes>
-      </Router>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
